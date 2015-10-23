0346953542464a99d68592fed00c9557f3b544b4
ganeshjawahar <buzzganesh@gmail.com>
2015-10-23 11:28:35 +0530

    First commit

---
+++ code/caffe.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Make sure that caffe is on the python path:
+caffe_root = '/home/adminuser/deep-learning/caffe'  # this file is expected to be in {caffe_root}/examples
+import sys
+sys.path.insert(0, caffe_root + 'python')
+
+import caffe
+
+plt.rcParams['figure.figsize'] = (10, 10)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+caffe.set_mode_cpu()
+net = caffe.Net(caffe_root + 'models/bvlc_alexnet/deploy.prototxt',
+                caffe_root + 'models/bvlc_alexnet/bvlc_alexnet.caffemodel',
+                caffe.TEST)
+
+# input preprocessing: 'data' is the name of the input blob == net.inputs[0]
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_transpose('data', (2,0,1))
+transformer.set_mean('data', np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1)) # mean pixel
+transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
+transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
